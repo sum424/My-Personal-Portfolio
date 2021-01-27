f56c49ed03ff3d918cb4b2caeecc706806bfbb7a
Sum424 <sumanthapa326@gmail.com>
2021-01-28 00:05:27 +0545

    added burger nav animation

--- script/script.js
+++ script/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-    $(".burger-nav").on("click", function() {
+   /*  $(".burger-nav").on("click", function() {
         $(".navbar ul").toggleClass("open");
-    })
+    }); */
+    //hamburger menu
+    var width = $(window).width();
+    if(width <= 420) {
+        $(".navbar").click(function() {
+            $(".icon").toggleClass("close");
+            $(".navbar ul").toggleClass("ul-disp").slideToggle(500);
+        });
+    }
+    if(width >420) {
+        $(".icon").removeClass("close");
+        $(".navbar ul").removeClass("ul-disp");
+    }
+
 })
